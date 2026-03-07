1aaff4d5e833bdeb4713ea53898caeef677ee9a7
Hiba <boudiajhiba2@gmail.com>
2026-03-07 20:35:13 +0000

    update transaction routes

--- src/routes/transaction.routes.js
+++ src/routes/transaction.routes.js
@@ -7,11 +7,5 @@ router.post("/", controller.create);
 
 router.get("/", controller.getAll);
 
-router.get("/:id", controller.getOne);
-
-router.put("/:id", controller.update);
-
-router.delete("/:id", controller.remove);
-
 module.exports = router;
 
